16dacb8359e79e5f421965e8cbcf6af8cc6b7024
akash yadav <64635004+akash4142@users.noreply.github.com>
2025-03-21 21:43:02 -0600

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://shield-security-backend.onrender.com' || 'http://localhost:5000/api';
+const API_BASE_URL = 'https://shield-security-backend.onrender.com' ;
 //const API_BASE_URL = 'http://localhost:5000/api';
 
 
