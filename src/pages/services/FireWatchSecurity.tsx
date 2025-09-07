295603e00371854211dc49a7440ba530ab313f97
akash yadav <64635004+akash4142@users.noreply.github.com>
2025-09-07 22:01:54 +0530

    07-9-2025

--- src/pages/services/FireWatchSecurity.tsx
+++ src/pages/services/FireWatchSecurity.tsx
@@ -26,10 +26,10 @@ const FireWatchSecurity = () => {
       <Box
         sx={{
           position: "relative",
-          height: "100vh",
+          minHeight:{xs:"100svh",md:"100vh"},
           backgroundImage: "url(/images/FireWatch.jpg)", // Make sure to place a Fire Watch related image
           backgroundSize: "cover",
-          backgroundAttachment: "fixed",
+          backgroundAttachment: { xs: "scroll", md: "fixed" },
           backgroundPosition: "center",
           display: "flex",
           alignItems: "center",
